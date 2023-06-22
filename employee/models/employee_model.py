f0534b1842f2aee7ecba2d790837f1a686ebba67
heshamsayed <hesham.sayed@arkleap.com>
2023-06-22 18:20:00 +0300

    test precommit

--- employee/models/employee_model.py
+++ employee/models/employee_model.py
@@ -1,15 +1,14 @@
 from django.db import models
+
 from people_mate.users.models import User
+
+
+# TODO pay attention to the spaces
 class Employee(models.Model):
-    name=models.CharField(max_length=60,verbose_name='Employee Name')
-    email=models.CharField(max_length=20,verbose_name='Email')
-    mobile=models.IntegerField(verbose_name='Mobile')
-    created_by=models.ForeignKey(User,on_delete=models.SET_NULL,related_name='created_employees',null=True)
-    updated_by=models.ForeignKey(User,on_delete=models.SET_NULL,related_name='updated_employees',null=True)
-    created_at=models.DateTimeField(auto_now_add=True)
-    updated_at=models.DateTimeField(null=True)
-
-
-    
-
-
+    name = models.CharField(max_length=60, verbose_name="Employee Name")
+    email = models.CharField(max_length=20, verbose_name="Email")
+    mobile = models.IntegerField(verbose_name="Mobile")
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, related_name="created_employees", null=True)
+    updated_by = models.ForeignKey(User, on_delete=models.SET_NULL, related_name="updated_employees", null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(null=True)
