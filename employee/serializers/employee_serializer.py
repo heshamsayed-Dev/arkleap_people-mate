d21e32ea070f7250db55e7ad4fc158418a0f7246
MohamedYehia12 <mohamed.yahia@arkleap.com>
2023-06-22 16:59:55 +0300

    remove user serializer from employee serializer

--- employee/serializers/employee_serializer.py
+++ employee/serializers/employee_serializer.py
@@ -2,7 +2,6 @@ from rest_framework import serializers
 from employee.models.employee_model import Employee
 from people_mate.users.api.serializers import UserSerializer
 class EmployeeSerializer(serializers.ModelSerializer):
-    created_by = UserSerializer()
     class Meta:
       model=Employee  
       fields = '__all__'
