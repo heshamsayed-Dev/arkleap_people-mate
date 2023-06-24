603d98cf9db8f233b775f8679630ddc6af9e7adc
heshamsayed <hesham.sayed@arkleap.com>
2023-06-24 19:33:49 +0300

    register apps in admin

--- employee/admin.py
+++ employee/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models.company_branch_model import CompanyBranch
+from .models.company_model import Company
+from .models.employee_model import Employee
+
 # Register your models here.
+
+admin.site.register(Company)
+admin.site.register(CompanyBranch)
+admin.site.register(Employee)
