ae6e70ff5f96192e417fb5b7fee8d545992c976f
MohamedYehia12 <mohamed.yahia@arkleap.com>
2023-07-11 14:48:04 +0000

    edits to attendance feature

--- attendance/apps.py
+++ attendance/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class AttendanceConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "attendance"
-
-    # def ready(self):
-    #     import attendance.signals
--- attendance/views/attendance_detail_create_view.py
+++ attendance/views/attendance_detail_create_view.py
@@ -1,21 +1,15 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from django.db.models.signals import pre_save
 
 from attendance.serializers.attendance_detail_serializer import AttendanceDetailSerializer
 
 
 class AttendanceDetailCreateView(APIView):
     def post(self, request):
-        # data = request.data.copy()
-        # print(f"employee branch is {request.user.employee.branch}")
-        # data['branch']=request.user.employee.branch
-        # print(data)
-        serializer = AttendanceDetailSerializer(data=request.data,context={"employee":request.user.employee})
+        serializer = AttendanceDetailSerializer(data=request.data, context={"employee": request.user.employee})
         if serializer.is_valid():
             serializer.save()
-           
 
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
