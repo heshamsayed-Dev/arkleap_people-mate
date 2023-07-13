0977d0bec94658825b7141d6bfc129a76fe17247
heshamsayed <hesham.sayed@arkleap.com>
2023-07-13 11:44:25 +0300

    code edits

--- policy/views/policy_api_view.py
+++ policy/views/policy_api_view.py
@@ -7,51 +7,35 @@ from rest_framework.response import Response
 
 # from people_mate.users.models import User
 from ..models.policy_model import Policy
-from ..serializers.policy_serializer import policySerializer
+from ..serializers.policy_serializer import policyserializer
 
 
 class PolicyViewSet(viewsets.ViewSet):
     queryset = Policy.objects.all()
     model = Policy
-    serializer_class = policySerializer
+    serializer_class = policyserializer
     pagination_class = LimitOffsetPagination
 
     # permission_classes = (IsAuthenticated,)
 
     def list(self, request):
-        # queryset = self.queryset.order_by("-created_at")
-        # serializer = self.serializer_class(queryset, many=True)
-        # data = {"success": True, "data": serializer.data,"count":queryset.count()}
-        # return Response(data, status=status.HTTP_200_OK)
-
         queryset = self.queryset.order_by("-created_at")
         paginator = self.pagination_class()
         objects = paginator.paginate_queryset(queryset, request)
         serializer = self.serializer_class(objects, many=True)
-        # dataa = {"success": True, "data": paginator.get_paginated_response(serializer.data),"count":queryset.count()}
-        # return Response(dataa, status=status.HTTP_200_OK)
         return paginator.get_paginated_response(serializer.data)
 
     def create(self, request):
         try:
             if request.method == "POST":
                 user = request.user
-                serialezer = self.serializer_class(data=request.data, context={"user": user})
-                if serialezer.is_valid():
-                    # x= str(serialezer.validated_data.get("working_policy_start_date"))
-                    # y= str(serialezer.validated_data.get("working_policy_end_date"))
 
-                    # start = datetime.strptime(x, "%H:%M:%S")
-                    # end = datetime.strptime(y, "%H:%M:%S")
-
-                    # print(end - start)
-
-                    serialezer.save()
-                    data = {"success": True, "data": serialezer.data}
-                    return Response(data, status=status.HTTP_201_CREATED)
+                serializer = self.serializer_class(data=request.data, context={"user": user})
+                if serializer.is_valid():
+                    serializer.save()
+                    return Response({"success": True, "data": serializer.data}, status=status.HTTP_201_CREATED)
                 else:
-                    data = {"success": True, "data": serialezer.errors}
-                    return Response(data, status=status.HTTP_400_BAD_REQUEST)
+                    return Response({"success": True, "data": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
         except Exception as e:
             return Response({"message": f"error:{e}, please connect to admin"}, status=status.HTTP_400_BAD_REQUEST)
 
@@ -59,14 +43,12 @@ class PolicyViewSet(viewsets.ViewSet):
         user = request.user
         try:
             instance = self.queryset.get(pk=pk)
-            serialezer = self.serializer_class(instance, request.data, context={"user": user})
-            if serialezer.is_valid():
-                serialezer.save()
-                data = {"success": True, "data": serialezer.data}
-                return Response(data, status=status.HTTP_200_OK)
+            serializer = self.serializer_class(instance, request.data, context={"user": user})
+            if serializer.is_valid():
+                serializer.save()
+                return Response({"success": True, "data": serializer.data}, status=status.HTTP_200_OK)
             else:
-                data = {"success": True, "data": serialezer.errors}
-                return Response(data, status=status.HTTP_400_BAD_REQUEST)
+                return Response({"success": True, "data": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
         except self.model.DoesNotExist:
             return Response(data="no policy with this id", status=status.HTTP_400_BAD_REQUEST)
         except self.model.MultipleObjectsReturned:
@@ -77,9 +59,8 @@ class PolicyViewSet(viewsets.ViewSet):
     def retrieve(self, request, pk=None):
         try:
             instance = self.queryset.get(pk=pk)
-            serialezer = self.serializer_class(instance)
-            data = {"success": True, "data": serialezer.data}
-            return Response(data, status=status.HTTP_200_OK)
+            serializer = self.serializer_class(instance)
+            return Response({"success": True, "data": serializer.data}, status=status.HTTP_200_OK)
         except self.model.DoesNotExist:
             return Response(data="no policy with this id", status=status.HTTP_400_BAD_REQUEST)
         except self.model.MultipleObjectsReturned:
