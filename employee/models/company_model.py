ed7d76210f189680d7ee2bd2cc6b9f5783e3248a
MohamedYehia12 <mohamed.yahia@arkleap.com>
2023-08-06 14:42:47 +0000

    adding enterprise fields and validations

--- employee/models/company_model.py
+++ employee/models/company_model.py
@@ -2,7 +2,17 @@ from django.db import models
 
 
 class Company(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=100)
+    arabic_name = models.CharField(max_length=100)
+    tax_number = models.BigIntegerField(unique=True)
+    commercial_record = models.BigIntegerField(unique=True)
     address = models.TextField()
+    phone = models.BigIntegerField()
+    mobile = models.CharField(null=True)
+    fax = models.BigIntegerField()
+    email = models.EmailField()
+    country = models.CharField(max_length=100)
+    start_date = models.DateField()
+    end_date = models.DateField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(null=True)
--- employee/serializers/company_serializer.py
+++ employee/serializers/company_serializer.py
@@ -1,7 +1,33 @@
+import re
+from datetime import datetime
+
 from rest_framework import serializers
+
 from employee.models.company_model import Company
+
+
 class CompanySerializer(serializers.ModelSerializer):
     class Meta:
-      model=Company  
-      fields = '__all__'
-   
+        model = Company
+        fields = "__all__"
+
+    def validate_arabic_name(self, value):
+        pattern = "[^ء-ي]ARABIC TEXT[^ء-ي]"
+        if not re.match(pattern, value):
+            raise serializers.ValidationError("arabic name must be in arabic")
+
+        return value
+
+    def validate_start_date(self, value):
+        today = datetime.today()
+        if value > today:
+            raise serializers.ValidationError("start date cannot be in the future")
+
+        return value
+
+    def validate_end_date(self, value):
+        today = datetime.today()
+        if value < today:
+            raise serializers.ValidationError("end date cannot be in the past")
+
+        return value
