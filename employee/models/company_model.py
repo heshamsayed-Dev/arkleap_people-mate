154f5c97d0fe8fa65270de0a4c3e5b3f792114c5
MohamedYehia12 <mohamed.yahia@arkleap.com>
2023-08-07 10:24:24 +0000

    adding slug in model company

--- employee/models/company_model.py
+++ employee/models/company_model.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.utils.text import slugify
 
 
 class Company(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
     arabic_name = models.CharField(max_length=100)
     tax_number = models.BigIntegerField(unique=True)
     commercial_record = models.BigIntegerField(unique=True)
@@ -14,5 +15,10 @@ class Company(models.Model):
     country = models.CharField(max_length=100)
     start_date = models.DateField()
     end_date = models.DateField(null=True)
+    slug = models.SlugField(null=False, unique=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(null=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        return super().save(*args, **kwargs)
--- employee/serializers/company_serializer.py
+++ employee/serializers/company_serializer.py
@@ -12,21 +12,21 @@ class CompanySerializer(serializers.ModelSerializer):
         fields = "__all__"
 
     def validate_arabic_name(self, value):
-        pattern = "[^ء-ي]ARABIC TEXT[^ء-ي]"
+        pattern = r"^[\u0621-\u063A\u0641-\u064A]+$"
         if not re.match(pattern, value):
             raise serializers.ValidationError("arabic name must be in arabic")
 
         return value
 
     def validate_start_date(self, value):
-        today = datetime.today()
+        today = datetime.today().date()
         if value > today:
             raise serializers.ValidationError("start date cannot be in the future")
 
         return value
 
     def validate_end_date(self, value):
-        today = datetime.today()
+        today = datetime.today().date()
         if value < today:
             raise serializers.ValidationError("end date cannot be in the past")
 
